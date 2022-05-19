065b05b0b9c53af1c119761a71eda41246b0caef
Josh Newburn <newburn2@illinois.edu>
2022-05-19 06:58:51 -0500

    deleting lab_7, didn't work

--- 385/lab_7/week2_code/software/lab71test/obj/default/RemoteSystemsTempFiles/.metadata/.plugins/org.eclipse.cdt.make.core/specs.d
+++
@@ -1,2 +0,0 @@
-obj/default/RemoteSystemsTempFiles/.metadata/.plugins/org.eclipse.cdt.make.core/specs.o: \
- RemoteSystemsTempFiles/.metadata/.plugins/org.eclipse.cdt.make.core/specs.cpp
--- 385/lab_7/week2_code/software/lab71test/obj/default/RemoteSystemsTempFiles/lab71test/lab71test/.metadata/.plugins/org.eclipse.cdt.make.core/specs.d
+++
@@ -1,2 +0,0 @@
-obj/default/RemoteSystemsTempFiles/lab71test/lab71test/.metadata/.plugins/org.eclipse.cdt.make.core/specs.o: \
- RemoteSystemsTempFiles/lab71test/lab71test/.metadata/.plugins/org.eclipse.cdt.make.core/specs.cpp
